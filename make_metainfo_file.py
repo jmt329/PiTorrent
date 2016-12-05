f2438e01633c241a0e72744000571f49b2481dc3
swc63 <swc63@cornell.edu>
2016-12-04 23:47:21 -0500

    added tool to make metainfo files

---
+++ make_metainfo_file.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import hashlib
+import bencode
+
+PIECE_SIZE = 256000 # 256 kB fixed piece size
+
+if len(sys.argv) < 3:
+  print "Error: must provide file and server name"
+  sys.exit()
+filename = sys.argv[1]
+# Read file
+meta_info = {
+    'announce' : sys.argv[2],
+    'info' : {}
+    }
+length = 0
+pieces = ""
+with open(filename, 'rb') as f:
+  piece = f.read(PIECE_SIZE)
+  length += len(piece)
+  while piece != "":
+    # hash each piece with sha1 and store in meta_info
+    h = hashlib.sha1()
+    h.update(piece)
+    pieces += str(h.digest())
+    piece = f.read(PIECE_SIZE)
+    length += len(piece)
+
+meta_info['info'] = {
+    'length' : length,
+    'name' : filename,
+    'piece_length' : PIECE_SIZE,
+    'pieces' : pieces
+    }
+# Bencode and store to a file with same filename, new extension ".pt"
+b_meta_info = bencode.bencode(meta_info)
+
+with open(filename + ".pt", 'wb') as f:
+  f.write(b_meta_info)
