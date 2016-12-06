85a69025ee34f0d719ccc756dd7315c0d7a379da
James Talmage <jmt329@cornell.edu>
2016-12-06 01:04:11 -0500

    Added PieceStatus class

---
+++ PieceStatus.py
@@ -0,0 +1,41 @@
+from threading import Lock
+from bitarray  import bitarray
+
+class PieceStatus:
+    """ Keeps track if pieces have been downloaded or if they are currently being
+    requested """
+
+    def __init__(self, numPieces, haveFile=False):
+        self.pieces = [0] * numPieces # initally nothing is downloaded
+        if(haveFile):
+            # peer is coming in as seeder
+            self.pieces = [2] * numPieces
+        self.lock = Lock()
+
+    # returns an empty piece number that has not been downloaded and is not
+    # currently being downloaded
+    # if no empty pieces exist, return None
+    def get_piece(self):
+        with self.lock:
+            try:
+                p = self.pieces.index(0)
+                self.pieces[p] = 1
+            except ValueError:
+                p = None
+            return p
+
+    # maks piece pieceNum as completed and verified
+    def finished_piece(self, pieceNum):
+        with self.lock:
+            self.pieces[pieceNum] = 2
+
+    # returns the bitarray representing the bitfield
+    def get_bitfield(self):
+        with self.lock:
+            bf = bitarray(len(pieces))
+            for i in xrange(len(pieces)):
+                if(self.pieces[i] == 2):
+                    bf[i] = 1
+                else:
+                    bf[i] = 0
+            return bf
